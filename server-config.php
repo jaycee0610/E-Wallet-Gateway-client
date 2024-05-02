c8a4dc45b528b4e19cae049bae190c37aa039c20
John Christian Natividad <120337618+jaycee0610@users.noreply.github.com>
2024-05-02 20:16:18 +0800

    Server Config

---
+++ server-config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Rootscratch E-Wallet Gateway (Client)
+ * Developed By jaycee0610
+ * May 02, 2024
+ * Payment Gateway Server : Xendit PH
+ */
+
+
+//Gateway Server
+$gateway_server = "web_address_here";
+
+//Enter API Key here
+$api_key = "your_api_key";
+
